713da9f87e0f9c31b06b690ee32bc1e77ef3b537
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-06-20 20:44:58 +0100

    Enable automatic upgrades

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,22 @@
 
   programs.git.enable = true;
 
+  # Automatic upgrades
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--recreate-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "07:30";
+    allowReboot = true;
+    rebootWindow = {
+      lower = "07:30";
+      upper = "09:00";
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
