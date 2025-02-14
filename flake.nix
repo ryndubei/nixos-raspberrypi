1bade490c2ab0a614e97b580e0dc48155f5ccd04
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-14 19:09:05 +0000

    Fix git default

--- flake.nix
+++ flake.nix
@@ -47,8 +47,11 @@
       };
 
       git-cfg = {
-        programs.git.userName = "ryndubei";
-        programs.git.userEmail = "114586905+ryndubei@users.noreply.github.com";
+        home-manager.users.raspbius = {
+          programs.git.userName = "ryndubei";
+          programs.git.userEmail =
+            "114586905+ryndubei@users.noreply.github.com";
+        };
       };
     in {
       nixosConfigurations = {
