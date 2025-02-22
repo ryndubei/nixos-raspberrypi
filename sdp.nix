cdf717ed524c360c5231f79cab65f3ebdf92bf6e
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-22 16:49:32 +0000

    Disable syncthing on sdp config

--- sdp.nix
+++ sdp.nix
@@ -11,6 +11,9 @@
   # SSH through student.ssh.inf.ed.ac.uk -> sdp-ranger
   services.tailscale.enable = lib.mkForce false;
 
+  # syncthing will use ~6% of RAM otherwise
+  home-manager.users.raspbius.services.syncthing.enable = lib.mkForce false;
+
   # Enable password login over the terminal
   users.users.raspbius.password = "group13";
   console.enable = lib.mkForce true;
