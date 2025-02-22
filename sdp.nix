126ffac161e1aea2a2c471cd7cdd2d6f7d371837
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-22 15:12:29 +0000

    Auto connect to SDProbots

--- sdp.nix
+++ sdp.nix
@@ -5,6 +5,8 @@
   networking.wireless.userControlled.enable = true;
   networking.wireless.allowAuxiliaryImperativeNetworks = true;
 
+  networking.wireless.networks."SDProbots".psk = "robotsRus";
+
   # don't think inf would react well to tailscale, will have to just
   # SSH through student.ssh.inf.ed.ac.uk -> sdp-ranger
   services.tailscale.enable = lib.mkForce false;
