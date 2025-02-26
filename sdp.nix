1e4d63afbedd2d0b209d50f7bc4072ea3c56abc6
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-26 13:40:32 +0000

    Use prebuilt image for ROS

--- sdp.nix
+++ sdp.nix
@@ -3,6 +3,19 @@
 let
   ssh-keys = config.users.users.raspbius.openssh.authorizedKeys.keys;
 
+  # note:
+  # https://discourse.nixos.org/t/running-nix-os-containers-directly-from-the-store-with-podman/29220
+  # podman run -ti --rm -v /nix/store:/nix/store --rootfs ./result:O /bin/hello
+  rosImage = pkgs.dockerTools.pullImage {
+    imageName = "ros";
+    imageDigest =
+      "sha256:ed1544e454989078f5dec1bfdabd8c5cc9c48e0705d07b678ab6ae3fb61952d2";
+    sha256 = "1bc8s1j9311qwg748s2qjjmr5f8yz14j5hhf63azc2l7kswmfq98";
+    finalImageName = "ros";
+    finalImageTag = "humble-perception";
+    arch = "arm64";
+  };
+
   adminUser = name: {
     users.users.${name} = {
       isNormalUser = true;
@@ -34,7 +47,14 @@ in {
     dockerCompat = true;
   };
 
-  environment.systemPackages = with pkgs; [ distrobox ];
+  environment.systemPackages = with pkgs; [
+    distrobox
+    podman-tui
+    (writeShellScriptBin "enter-ros-shell" ''
+      set -euxo pipefail
+      ${podman}/bin/podman run -ti --rm -v /nix/store:/nix/store -v "$HOME":"/home/$USER" --user "$USER" --rootfs ${rosImage}:O ${bash}/bin/bash
+    '')
+  ];
 
   # Enable password login over the terminal
   users.users.raspbius.password = "group13";
