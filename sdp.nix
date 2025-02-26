f91acd4f32e28f7424fe004837cb72f01d8b98a5
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-26 13:43:01 +0000

    Use hotspot instead of SDProbots

--- sdp.nix
+++ sdp.nix
@@ -22,19 +22,7 @@ let
 in {
   networking.wireless.enable = true;
 
-  networking.wireless.networks."SDProbots".psk = "robotsRus";
-
-  # SDProbots has no DHCP, must use static IP
-  networking.interfaces.wlan0.useDHCP = false;
-  networking.interfaces.wlan0.ipv4.addresses = [{
-    address = "192.168.105.169";
-    prefixLength = 24;
-  }];
-  networking.defaultGateway = "192.168.105.243";
-
-  # don't think inf would react well to tailscale, will have to just
-  # SSH through student.ssh.inf.ed.ac.uk -> sdp-ranger
-  services.tailscale.enable = lib.mkForce false;
+  networking.wireless.networks."ranger-hotspot".psk = "12345678";
 
   # syncthing will use ~6% of RAM otherwise
   home-manager.users.raspbius.services.syncthing.enable = lib.mkForce false;
